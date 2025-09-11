3a97df4279d5f335975c942b63880275ce0c0295
mohamedpedagogy <mohamed@pedagogy.cloud>
2025-09-11 20:32:22 +0530

    Learned loops

---
+++ 11-09-2025.py
@@ -0,0 +1,20 @@
+print('I am a best')
+
+# For loops
+
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+  print(x)
+
+for x in range(1, 9):
+  print(x)
+
+# While loops
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
